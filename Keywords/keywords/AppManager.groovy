fc43768e2a9c75ffc8cbbdc5b8cc322dfb4d813c
MuchoRio <pucuk4us@gmail.com>
2026-01-30 00:53:19 +0700

    feat: initial commit

---
+++ Keywords/keywords/AppManager.groovy
@@ -0,0 +1,39 @@
+package keywords
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.util.KeywordUtil
+import keywords.MobileBaseConfig
+
+import internal.GlobalVariable
+
+public class AppManager {
+	
+	@Keyword
+	static void launchApp() {
+		def config = MobileBaseConfig.getMobileConfig()
+		try {
+			KeywordUtil.logInfo("Aplikasi sedang Berjalan ${config.APP_PACKAGE}")
+			Mobile.delay(config.DELAY_DURATION)
+			Mobile.startExistingApplication(config.APP_PACKAGE, FailureHandling.STOP_ON_FAILURE)
+			KeywordUtil.markPassed("Aplikasi Berjalan Sempurna, melanjutkan ke Test Casenya!")
+		} catch (Exception e) {
+			KeywordUtil.markError("Aplikasi Tidak Ditemukan: ${config.APP_PACKAGE}" + e.message)
+		}
+	}
+}
---
+++ Keywords/keywords/MobileBaseConfig.groovy
@@ -0,0 +1,37 @@
+package keywords
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class MobileBaseConfig {
+	
+	static Map<String, Object> getMobileConfig() {
+		
+		if(!GlobalVariable.G_AppPackage) {
+			KeywordUtil.logInfo("App Gak Berjalan!!")
+		}
+		
+		return [
+			"APP_PACKAGE"	: GlobalVariable.G_AppPackage?.toString()?.trim() ?: 'in.sweatco.app',
+			"DELAY_DURATION": (GlobalVariable.G_DelayDuration ?: 3) as Integer
+		]
+	}
+}
